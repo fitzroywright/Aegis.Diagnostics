c3e2aca7680801f3aeff112f8a400d59ae503829
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-11 02:11:36 -0500

    Fix Common.Messaging channel flags

--- Diagnostics/OrchestrationServices.cs
+++ Diagnostics/OrchestrationServices.cs
@@ -129,8 +129,8 @@ public sealed class CommonMessagingIncidentNotificationPublisher(IServiceProvide
 
         MessageChannel channels = run.Status switch
         {
-            EngineeringDiagnosticStatus.InterventionRequired => MessageChannel.InApp | MessageChannel.Email | MessageChannel.Slack | MessageChannel.Teams,
-            EngineeringDiagnosticStatus.Failed => MessageChannel.InApp | MessageChannel.Email | MessageChannel.Slack,
+            EngineeringDiagnosticStatus.InterventionRequired => MessageChannel.InApp | MessageChannel.MsEmail | MessageChannel.Slack | MessageChannel.MsTeams,
+            EngineeringDiagnosticStatus.Failed => MessageChannel.InApp | MessageChannel.MsEmail | MessageChannel.Slack,
             EngineeringDiagnosticStatus.Warning => MessageChannel.InApp,
             _ => MessageChannel.InApp
         };
